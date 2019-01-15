734ff2f6274ec581e3abef7b8ffa94499eeb0e3f
sanjuthomas <sanju@sanju.org>
2019-01-14 20:49:58 -0500

    adding the util classes

--- src/test/java/bigtable/utils/CreateTable.java
+++ src/test/java/bigtable/utils/CreateTable.java
@@ -1,25 +1,25 @@
 package bigtable.utils;
 
 import java.io.IOException;
-
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
 import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
 
 public class CreateTable {
-	
-	public void execute(String project, String instance, String table) throws IOException {
-		BigtableTableAdminSettings adminSettings = BigtableTableAdminSettings.newBuilder()
-				.setInstanceName(com.google.bigtable.admin.v2.InstanceName.of(project, instance))
-				.build();
-		try (BigtableTableAdminClient bigtableDataClient = BigtableTableAdminClient.create(adminSettings)) {
-			CreateTableRequest request = CreateTableRequest.of(table);
-			bigtableDataClient.createTable(request);
-		}
-	}
-	
-	//have the GOOGLE_APPLICATION_CREDENTIALS system environment variable set
-	public static void main(String[] args) throws IOException {
-		new CreateTable().execute("pcln-pl-cps-poc", "demo-instance", "demo-table");
-	}
+
+  public void execute(final String project, final String instance, final String table)
+      throws IOException {
+    final BigtableTableAdminSettings adminSettings = BigtableTableAdminSettings.newBuilder()
+        .setInstanceName(com.google.bigtable.admin.v2.InstanceName.of(project, instance)).build();
+    try (BigtableTableAdminClient bigtableDataClient =
+        BigtableTableAdminClient.create(adminSettings)) {
+      final CreateTableRequest request = CreateTableRequest.of(table);
+      bigtableDataClient.createTable(request);
+    }
+  }
+
+  // have the GOOGLE_APPLICATION_CREDENTIALS system environment variable set
+  public static void main(final String[] args) throws IOException {
+    new CreateTable().execute("demo-project", "demo-instance", "demo-table");
+  }
 }
