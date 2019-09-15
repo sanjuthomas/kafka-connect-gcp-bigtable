df4525bc51d724df0ce7bfa4c192dca598ae4e2c
sanjuthomas <sanju@sanju.org>
2019-09-14 20:58:05 -0400

    more test cases

--- src/test/java/com/sanjuthomas/gcp/bigtable/sink/BigtableSinkConnectorTest.java
+++ src/test/java/com/sanjuthomas/gcp/bigtable/sink/BigtableSinkConnectorTest.java
@@ -15,19 +15,19 @@ import org.junit.jupiter.api.Test;
  *
  */
 public class BigtableSinkConnectorTest {
-	
-	private BigtableSinkConnector bigtableSinkConnector;
-	
-	@BeforeEach
-	public void setup() {
-		bigtableSinkConnector = new BigtableSinkConnector();
-		bigtableSinkConnector.start(Collections.emptyMap());
-	}
 
-	@Test
-	public void shouldGetTaskConfigs() {
-		List<Map<String, String>> taskConfigs = bigtableSinkConnector.taskConfigs(1);
-		assertEquals(1, taskConfigs.size());
-		assertEquals(0, taskConfigs.get(0).size());
-	}
+  private BigtableSinkConnector bigtableSinkConnector;
+
+  @BeforeEach
+  public void setup() {
+    bigtableSinkConnector = new BigtableSinkConnector();
+    bigtableSinkConnector.start(Collections.emptyMap());
+  }
+
+  @Test
+  public void shouldGetTaskConfigs() {
+    List<Map<String, String>> taskConfigs = bigtableSinkConnector.taskConfigs(1);
+    assertEquals(1, taskConfigs.size());
+    assertEquals(0, taskConfigs.get(0).size());
+  }
 }
--- src/test/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriterTest.java
+++ src/test/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriterTest.java
@@ -1,53 +1,71 @@
 package com.sanjuthomas.gcp.bigtable.writer;
 
-import com.sanjuthomas.gcp.bigtable.bean.WritableRow;
-import com.sanjuthomas.gcp.bigtable.config.WriterConfig;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.concurrent.ExecutionException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import com.sanjuthomas.gcp.bigtable.config.ClientProvider;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.models.BulkMutation;
+import com.sanjuthomas.gcp.bigtable.bean.WritableRow;
 import com.sanjuthomas.gcp.bigtable.config.TransformerConfig;
+import com.sanjuthomas.gcp.bigtable.config.WriterConfig;
 import com.sanjuthomas.gcp.bigtable.transform.JsonEventTransformer;
 import com.sanjuthomas.gcp.resolvers.SinkRecordResolver;
-import com.sanjuthomas.gcp.bigtable.Integration;
 
-@Integration
+@ExtendWith(MockitoExtension.class)
 public class BigtableWriterTest {
 
-	private BigtableWriter writer;
-	private JsonEventTransformer transformer;
-	private final List<String> keyQualifiers = Arrays.asList(new String[] { "symbol" });
-	private final List<String> families = Arrays.asList(new String[] { "data", "metadata" });
-	private Map<String, List<String>> familyToQualifierMapping;
+  @Mock
+  private BigtableDataClient client;
+  
+  @Mock
+  private ApiFuture<Void> apiFuture;
+  
+  private BigtableWriter writer;
+  private JsonEventTransformer transformer;
+  private final List<String> keyQualifiers = Arrays.asList(new String[] {"symbol"});
+  private final List<String> families = Arrays.asList(new String[] {"data", "metadata"});
+  private Map<String, List<String>> familyToQualifierMapping;
 
-	@BeforeEach
-	public void setup() throws FileNotFoundException, IOException {
-		this.familyToQualifierMapping = new HashMap<>();
-		this.familyToQualifierMapping.put("data", Arrays.asList(new String[] { "symbol", "name", "sector" }));
-		this.familyToQualifierMapping.put("metadata",
-				Arrays.asList(new String[] { "create_time", "processing_time", "topic" }));
-		final TransformerConfig config = new TransformerConfig(this.keyQualifiers, "_", this.families,
-				this.familyToQualifierMapping);
-		this.transformer = new JsonEventTransformer(config);
-		WriterConfig writerConfig = new WriterConfig("/Users/sathomas/keys/demo-key.json", "demo-project",
-            "demo-instance", "demo-table");
-		this.writer = new BigtableWriter(writerConfig, new ClientProvider(writerConfig).client());
-	}
+  @BeforeEach
+  public void setup() throws FileNotFoundException, IOException {
+    this.familyToQualifierMapping = new HashMap<>();
+    this.familyToQualifierMapping.put("data",
+        Arrays.asList(new String[] {"symbol", "name", "sector"}));
+    this.familyToQualifierMapping.put("metadata",
+        Arrays.asList(new String[] {"create_time", "processing_time", "topic"}));
+    final TransformerConfig config = new TransformerConfig(this.keyQualifiers, "_", this.families,
+        this.familyToQualifierMapping);
+    this.transformer = new JsonEventTransformer(config);
+    WriterConfig writerConfig = new WriterConfig("/Users/sathomas/keys/demo-key.json",
+        "demo-project", "demo-instance", "demo-table");
+    this.writer = new BigtableWriter(writerConfig, client);
+  }
 
-	@Test
-	@ExtendWith(SinkRecordResolver.class)
-	public void shouldWrite(final SinkRecord record) {
-		final WritableRow row = this.transformer.transform(record);
-		writer.buffer(row);
-		writer.flush();
-	}
+  @Test
+  @ExtendWith(SinkRecordResolver.class)
+  public void shouldWrite(final SinkRecord record) throws InterruptedException, ExecutionException {
+    when(client.bulkMutateRowsAsync(any(BulkMutation.class))).thenReturn(apiFuture);
+    final WritableRow row = this.transformer.transform(record);
+    writer.buffer(row);
+    writer.flush();
+    verify(client, times(1)).bulkMutateRowsAsync(any(BulkMutation.class));
+    verify(apiFuture, times(1)).get();
+  }
 
 }
