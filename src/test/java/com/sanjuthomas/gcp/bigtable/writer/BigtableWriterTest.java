9f33adfc2639f6f16b8b76921e04d32dff14b5ee
sanjuthomas <sanju@sanju.org>
2019-09-14 21:16:34 -0400

    more test cases

--- src/test/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriterTest.java
+++ src/test/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriterTest.java
@@ -1,7 +1,6 @@
 package com.sanjuthomas.gcp.bigtable.writer;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
