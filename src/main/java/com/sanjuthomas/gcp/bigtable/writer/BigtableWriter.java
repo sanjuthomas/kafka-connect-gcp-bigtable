7e42be6b3b45b08999cb909c05fe65e668d62810
sanjuthomas <sanju@sanju.org>
2020-05-17 12:15:08 +0530

    fixing the java doc param typo

--- src/main/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriter.java
+++ src/main/java/com/sanjuthomas/gcp/bigtable/writer/BigtableWriter.java
@@ -25,7 +25,7 @@ import com.sanjuthomas.gcp.bigtable.writer.ErrorHandler.Result;
  * Default Bigtable writer implementation and this class is not thread safe. As per the design,
  * there would be one writer per topic and every task thread will get it's own writer instance.
  * 
- * a Task -> a Topic -> a Writer is the cardinality per design. So nothing is shared among tasks.
+ * Nothing is shared among tasks. A task gets a topic to read from and a writer to flush the messages out.
  * 
  * This implementation write rows using bulkMutateRows.
  * 
